7284cbb83ef3d084b04172bc5304678945fac138
roberteftene <roberteftene25@gmail.com>
2021-02-23 12:19:44 +0200

    excluded .idea files again

--- S1/src/ro/acs/ase/laboratory/HelloWorld.java
+++ S1/src/ro/acs/ase/laboratory/HelloWorld.java
@@ -3,7 +3,7 @@ package ro.acs.ase.laboratory;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Configuration");
+        System.out.println("Hello world");
     }
 
 }

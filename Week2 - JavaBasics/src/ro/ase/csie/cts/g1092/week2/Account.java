1ff2cc5ddf610fbf9bd2ca230223b85846a5d4a7
roberteftene <roberteftene25@gmail.com>
2021-03-02 11:29:42 +0200

    final commit

--- Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
+++ Week2 - JavaBasics/src/ro/ase/csie/cts/g1092/week2/Account.java
@@ -1,10 +1,13 @@
 package ro.ase.csie.cts.g1092.week2;
 
+import ro.ase.csie.cts.g1092.week2.exeptions.IllegalTranserException;
+import ro.ase.csie.cts.g1092.week2.exeptions.InsufficientFundsException;
+
 public abstract class Account {
 
     public abstract double getBalance();
     public abstract void deposit(double amount);
-    public abstract void transfer(double amount, Account destination);
-    public abstract void withdraw(double amount);
+    public abstract void transfer(double amount, Account destination) throws InsufficientFundsException, IllegalTranserException;
+    public abstract void withdraw(double amount) throws InsufficientFundsException;
 
 }

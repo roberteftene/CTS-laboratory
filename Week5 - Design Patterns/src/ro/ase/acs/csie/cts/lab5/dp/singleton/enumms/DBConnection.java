66cf15890dad23b787467c89e2f45bfbecba169c
roberteftene <roberteftene25@gmail.com>
2021-03-23 11:16:20 +0200

    Singleton Enum

--- Week5 - Design Patterns/src/ro/ase/acs/csie/cts/lab5/dp/singleton/enumms/DBConnection.java
+++ Week5 - Design Patterns/src/ro/ase/acs/csie/cts/lab5/dp/singleton/enumms/DBConnection.java
@@ -1,4 +1,24 @@
 package ro.ase.acs.csie.cts.lab5.dp.singleton.enumms;
 
 public enum DBConnection {
+    instance;
+
+    String socket;
+    String schema;
+
+    public String getSocket() {
+        return socket;
+    }
+
+    public void setSocket(String socket) {
+        this.socket = socket;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
 }
